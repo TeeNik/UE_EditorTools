171da9c412d562d8f53b0c4ec4c5830bdbae197a
TeeNik <teenik3315@gmail.com>
2023-08-02 04:23:12 +0600

    ClassSelector implementation

--- Source/EditorTools/ClassSelector.h
+++ Source/EditorTools/ClassSelector.h
@@ -1,5 +1,39 @@
 #pragma once
-class ClassSelector
+#include "CoreMinimal.h"
+#include "ClassSelector.generated.h"
+
+USTRUCT()
+struct FClassSelectorParams
 {
+	GENERATED_BODY()
+
+	UPROPERTY()
+	const UClass* Class = nullptr;
+
+	UPROPERTY()
+	bool Result = false;
 };
 
+// FClassSelector supports several meta specifiers:
+// SelectorClass - filter will show only children of SelectorClass
+// SelectorFunc - Function(FClassSelectorParams&) with name SelectorFunc will be called on property owner
+
+USTRUCT(BlueprintType)
+struct FClassSelector
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UClass> Class;
+
+	FClassSelector() {}
+
+	FClassSelector(TObjectPtr<UClass> InClass) : Class(InClass)
+	{
+	}
+
+	operator UClass* () const
+	{
+		return Class.Get();
+	}
+};
