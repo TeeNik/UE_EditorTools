e00a2ddee20cb01863d0008e7db9473219b8e2f8
TeeNik <teenik3315@gmail.com>
2023-08-12 22:21:35 +0300

    make base example class abstact

--- Source/EditorTools/WorldObjects/ExampleOptionActor.h
+++ Source/EditorTools/WorldObjects/ExampleOptionActor.h
@@ -1,12 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "ExampleOptionActor.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class EDITORTOOLS_API AExampleOptionActor : public AActor
 {
 	GENERATED_BODY()
