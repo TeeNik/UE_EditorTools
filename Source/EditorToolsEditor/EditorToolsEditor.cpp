8deb2e44a6e1f0467329bb7ac83f94a73cad07b0
TeeNik <teenik3315@gmail.com>
2023-08-13 22:22:17 +0300

    register customization class

--- Source/EditorToolsEditor/EditorToolsEditor.cpp
+++ Source/EditorToolsEditor/EditorToolsEditor.cpp
@@ -1,6 +1,7 @@
 #include "EditorToolsEditor.h"
 #include "Modules/ModuleManager.h"
 #include "Modules/ModuleInterface.h"
+#include "Utils/ClassSelectorCustomization.h"
 
 DEFINE_LOG_CATEGORY(EditorToolsEditor)
 
@@ -9,6 +10,11 @@ DEFINE_LOG_CATEGORY(EditorToolsEditor)
 void FEditorToolsEditorModule::StartupModule()
 {
     UE_LOG(EditorToolsEditor, Warning, TEXT("FEditorToolsEditor: Log Started"));
+
+	// Register the details customizer
+	FPropertyEditorModule& PropertyModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	PropertyModule.RegisterCustomPropertyTypeLayout("ClassSelector", FOnGetPropertyTypeCustomizationInstance::CreateStatic(&FClassSelectorCustomization::MakeInstance));
+	PropertyModule.NotifyCustomizationModuleChanged();
 }
 
 void FEditorToolsEditorModule::ShutdownModule()
