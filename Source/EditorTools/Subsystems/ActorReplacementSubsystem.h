2b485b1c0e278e8b0d57ce04a5c24f717ea792f5
TeeNik <teenik3315@gmail.com>
2023-12-30 00:49:38 +0100

    add replacement subsystem

---
+++ Source/EditorTools/Subsystems/ActorReplacementSubsystem.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorSubsystem.h"
+#include "ActorReplacementSubsystem.generated.h"
+
+
+UCLASS()
+class EDITORTOOLS_API UActorReplacementSubsystem : public UEditorSubsystem
+{
+	GENERATED_BODY()
+	
+};
