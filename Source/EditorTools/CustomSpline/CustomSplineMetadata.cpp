5ef9ecf9d0c2e3f53f85d45b2da4e6959c3e4e1e
TeeNik <teenik3315@gmail.com>
2024-05-30 01:58:33 +0200

    CustomSplineMetadata logic

--- Source/EditorTools/CustomSpline/CustomSplineMetadata.cpp
+++ Source/EditorTools/CustomSpline/CustomSplineMetadata.cpp
@@ -3,3 +3,69 @@
 
 #include "CustomSplineMetadata.h"
 
+void UCustomSplineMetadata::InsertPoint(int32 Index, float t, bool bClosedLoop)
+{
+	if (Index >= PointParams.Num())
+	{
+		AddPoint(static_cast<float>(Index));
+	} else
+	{
+		PointParams.Insert(FCustomSplinePointParams{}, Index);
+	}
+	
+	Modify();
+}
+
+void UCustomSplineMetadata::UpdatePoint(int32 Index, float t, bool bClosedLoop)
+{
+	Modify();
+}
+
+void UCustomSplineMetadata::AddPoint(float InputKey)
+{
+	PointParams.Add(FCustomSplinePointParams{});
+	Modify();
+}
+
+void UCustomSplineMetadata::RemovePoint(int32 Index)
+{
+	PointParams.RemoveAt(Index);
+	Modify();
+}
+
+void UCustomSplineMetadata::DuplicatePoint(int32 Index)
+{
+	FCustomSplinePointParams NewVal = PointParams[Index];
+	PointParams.Insert(NewVal, Index);
+	Modify();
+}
+
+void UCustomSplineMetadata::CopyPoint(const USplineMetadata* FromSplineMetadata, int32 FromIndex, int32 ToIndex)
+{
+	if (const UCustomSplineMetadata* FromMetadata = Cast<UCustomSplineMetadata>(FromSplineMetadata))
+	{
+		PointParams[ToIndex] = PointParams[FromIndex];
+		Modify();
+	}
+}
+
+void UCustomSplineMetadata::Reset(int32 NumPoints)
+{
+	PointParams.Reset(NumPoints);
+	Modify();
+}
+
+void UCustomSplineMetadata::Fixup(int32 NumPoints, USplineComponent* SplineComp)
+{
+	if (PointParams.Num() > NumPoints)
+	{
+		PointParams.RemoveAt(NumPoints, PointParams.Num() - NumPoints);
+		Modify();
+	}
+
+	while (PointParams.Num() < NumPoints)
+	{
+		PointParams.Add(FCustomSplinePointParams{});
+		Modify();
+	}
+}
--- Source/EditorTools/CustomSpline/CustomSplineMetadata.h
+++ Source/EditorTools/CustomSpline/CustomSplineMetadata.h
@@ -3,15 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CustomSplineComponent.h"
 #include "Components/SplineComponent.h"
 #include "CustomSplineMetadata.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class EDITORTOOLS_API UCustomSplineMetadata : public USplineMetadata
 {
 	GENERATED_BODY()
+
+public:
+	virtual void InsertPoint(int32 Index, float t, bool bClosedLoop) override;
+	virtual void UpdatePoint(int32 Index, float t, bool bClosedLoop) override;
+	virtual void AddPoint(float InputKey) override;
+	virtual void RemovePoint(int32 Index) override;
+	virtual void DuplicatePoint(int32 Index) override;
+	virtual void CopyPoint(const USplineMetadata* FromSplineMetadata, int32 FromIndex, int32 ToIndex) override;
+	virtual void Reset(int32 NumPoints) override;
+	virtual void Fixup(int32 NumPoints, USplineComponent* SplineComp) override;
+
+	UPROPERTY(EditAnywhere)
+	TArray<FCustomSplinePointParams> PointParams;
 	
 };
